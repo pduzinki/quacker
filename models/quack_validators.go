7ccbb5d8ce5e3e3a4e51fb95ec996a9ef85f83e0
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2020-08-02 19:56:00 +0200

    add first quack validator

--- models/quack_validators.go
+++ models/quack_validators.go
@@ -1,1 +1,20 @@
 package models
+
+type quackValidatorFunc func(*Quack) error
+
+func runQuackValidatorFuncs(quack *Quack, fns ...quackValidatorFunc) error {
+	for _, f := range fns {
+		err := f(quack)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (qv *quackValidator) idGreaterThanZero(quack *Quack) error {
+	if quack.ID <= 0 {
+		return ErrInvalidID
+	}
+	return nil
+}
