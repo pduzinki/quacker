2c362a1056470803afa30ab95e12ef1a67b456db
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2020-08-16 12:22:10 +0200

    add saving user data to context in user require middleware

--- middleware/user_require.go
+++ middleware/user_require.go
@@ -4,17 +4,18 @@ import (
 	"fmt"
 	"net/http"
 
+	"quacker/context"
 	"quacker/models"
 )
 
-// UserRequire TODO add description ...
+// UserRequire is a middleware that verifies that there is a user logged in,
+// and if not, it redirects to "/login" page
 type UserRequire struct {
 	models.UserService
 }
 
 // ApplyFn TODO add description ...
 func (mw *UserRequire) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cookie, err := r.Cookie("remember_token")
 		if err != nil {
@@ -27,6 +28,10 @@ func (mw *UserRequire) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
 			http.Redirect(w, r, "/login", http.StatusFound)
 			return
 		}
+
+		ctx := r.Context()
+		ctx = context.WithUser(ctx, user)
+		r = r.WithContext(ctx)
 
 		fmt.Println("User found:", user)
 		next(w, r)
